c8ea762a77245de2a0a0a9f24ecb8b402dca0882
YisakH <drm05003@naver.com>
2024-12-12 15:19:24 +0900

    add dockerfile

--- LogServer/src/main/java/com/hungry/logServer/Alert/DiscordAlerter.java
+++ LogServer/src/main/java/com/hungry/logServer/Alert/DiscordAlerter.java
@@ -1,7 +1,6 @@
 package com.hungry.logServer.Alert;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
